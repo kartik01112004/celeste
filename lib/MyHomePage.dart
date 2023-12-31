76fbd869fd69b8f31b325637b1727753c2e660e5
vanshika704 <sharmavanshi704@gmail.com>
2023-12-31 12:01:28 +0530

    commit

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -14,50 +14,55 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/earth1.jpg"),
-                  fit: BoxFit.cover,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/earth1.jpg",
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "WINTER HACKS",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 241, 239, 239),
+                    fontSize: 30,
+                  ),
                 ),
-              ),
-              child: const Text(
-                "WINTER HACKS",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 241, 239, 239),
-                  fontSize: 30,
+                const SizedBox(height: 16),
+                const Text(
+                  "DIVE INTO AN IMMERSIVE TRAVELLING EXPERIENCE",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(LOGINPAGE());
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(97, 168, 164, 164),
+                  ),
+                  child: Text(
+                    "PLAY NOW",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Get.to(SIGNUPPAGE());
+                  },
+                  color: Colors.white,
+                  icon: Icon(Icons.person_add_outlined),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            const Text(
-              "DIVE INTO AN IMMERSIVE TRAVELLING EXPERIENCE",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Get.to(LOGINPAGE());
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(97, 168, 164, 164),
-              ),
-              child: Text("PLAY NOW"),
-            ),
-            IconButton(
-                onPressed: () {
-                  Get.to(SIGNUPPAGE());
-                },
-                color: Colors.white,
-                icon: Icon(Icons.person_add_outlined))
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
